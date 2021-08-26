f9a221a934c95ccf2f70b773fcf1121e02f84437
systemdma <dystopianworld@protonmail.com>
2021-08-26 22:46:10 +0300

    Add possible use firefox instead chrome, update readme file, add requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # multilingual-interface-test
-This is learn test.
+This is learn test. Site: http://selenium1py.pythonanywhere.com/
+Test is task from course: https://stepik.org/course/575/syllabus
+Test execute from pytest.
+
+Using:
+>**pytest --language=** ru, fr, en-gb, ... (must) **--browser=** *chrome/firefox* (not must)
--- conftest.py
+++ conftest.py
@@ -7,11 +7,22 @@ def pytest_addoption(parser):
         "--language",
         default=None,
         help="choose language")
+    parser.addoption(
+        "--browser",
+        default="chrome",
+        help="choose browser: chrome or firefox"
+    )
+
 
 
 @pytest.fixture
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    if request.config.getoption("--browser") == "chrome":
+        browser = webdriver.Chrome()
+    elif request.config.getoption("--browser") == "firefox":
+        browser = webdriver.Firefox()
+    else:
+        raise ValueError("Uncorrect browser name")
     yield browser
     browser.quit()
 
