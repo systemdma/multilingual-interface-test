4fb67609470726469577c87a6b9110185e7e6fdf
systemdma <dystopianworld@protonmail.com>
2021-08-26 22:53:33 +0300

    Add pause (need for review)

--- test_items.py
+++ test_items.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium.webdriver.common.by import By
+import time
 
 
 def test_exist_basket_button(browser, get_choose_language):
@@ -9,4 +10,5 @@ def test_exist_basket_button(browser, get_choose_language):
     else:
         url = f"http://selenium1py.pythonanywhere.com/{lang}/catalogue/coders-at-work_207/"
         browser.get(url)
+        time.sleep(30)
         assert browser.find_element_by_class_name("btn-add-to-basket"), "basket button not found"
